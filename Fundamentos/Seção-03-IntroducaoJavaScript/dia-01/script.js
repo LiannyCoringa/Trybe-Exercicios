d9377d0ce6c61b9197a4217b01754e238ec53b78
Lianny Coringa <lianny_costa@hotmail.com>
2023-03-28 10:18:13 -0300

    Exercício 02 sobre operadores aritméticos

--- Fundamentos/Seção-03-IntroducaoJavaScript/dia-01/script.js
+++ Fundamentos/Seção-03-IntroducaoJavaScript/dia-01/script.js
@@ -1,4 +1,10 @@
 let myName = "Lianny";
 let birthCity = "Natal";
 let birthYear = 1998;
-console.log(myName, birthCity, birthYear)
+// console.log(myName, birthCity, birthYear)
+
+let base = 5;
+let height = 8;
+let area = base * height;
+let perimeter = (base * 2) + (height * 2);
+console.log(base, height, area, perimeter)
