d9b01134f5e66857344ff811cd68b3fe552e2900
Lianny Coringa <lianny_costa@hotmail.com>
2023-03-28 15:27:57 -0300

    Exercícios 1 ao 4

---
+++ Fundamentos/Seção-03-IntroducaoJavaScript/dia-01/script1.js
@@ -0,0 +1,26 @@
+// let a = 9;
+// let b = 5;
+// let c = 24;
+
+// if (a > b){
+//     console.log(`O valor ${a} é maior`);
+// } else {
+//     console.log(`O valor ${b} é maior`);
+// }
+
+// if (a > b && a > c) {
+//     console.log(`O valor ${a} é maior`);
+// } else if (b > a && b > c) {
+//     console.log(`O valor ${b} é maior`);
+// } else {
+//     console.log(`O valor ${c} é maior`);
+// }
+
+let value = 0;
+if (value > 0) {
+    console.log('Positivo');
+} else if (value < 0) {
+    console.log('Negativo');
+} else {
+    console.log('Zero');
+}
