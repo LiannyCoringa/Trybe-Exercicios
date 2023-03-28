7e509bae61514341da3f6b3d5624369f4d275206
Lianny Coringa <lianny_costa@hotmail.com>
2023-03-28 10:30:21 -0300

    Exercício 03 sobre if e else

--- Fundamentos/Seção-03-IntroducaoJavaScript/dia-01/script.js
+++ Fundamentos/Seção-03-IntroducaoJavaScript/dia-01/script.js
@@ -7,4 +7,13 @@ let base = 5;
 let height = 8;
 let area = base * height;
 let perimeter = (base * 2) + (height * 2);
-console.log(base, height, area, perimeter)
+// console.log(base, height, area, perimeter)
+
+let nota = 65;
+if (nota >= 80){
+    console.log('Parabéns, você faz parte do grupo de pessoas aprovadas!');
+} else if (nota < 80 && nota >= 60){
+    console.log('Você está na nossa lista de espera.');
+} else {
+    console.log('Infelizmente, você reprovou.');
+}
