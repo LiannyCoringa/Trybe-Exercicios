2d6709f05cf0dcd8aa79e7f6d51cc7fb054bbbc9
Lianny Coringa <lianny_costa@hotmail.com>
2023-03-29 11:01:48 -0300

    Exercícios do dia 2

---
+++ Fundamentos/Seção-03-IntroducaoJavaScript/dia-02/README.md
@@ -0,0 +1,7 @@
+Descrição dos exercícios:
+
+1. script.js:
+    Obtenha o valor "Serviços" do array menu; Procure o índice do valor “Portfólio” do array menu; Adicione o valor “Contato” no final do array menu.
+
+1. script01.js:
+    Utilize o for para imprimir os elementos da lista groceryList com o console.log(); Utilize o for/of para imprimir os elementos da lista names com o console.log().
---
+++ Fundamentos/Seção-03-IntroducaoJavaScript/dia-02/script02.js
@@ -0,0 +1,19 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let sum = 0;
+
+
+// for (let number of numbers){
+//     // console.log(number);
+//     // sum += number;
+// }; 
+// console.log(sum);
+// console.log(sum / numbers.length)
+
+// let media = sum / numbers.length;
+// if (media > 20) {
+//     console.log('Valor maior que 20');
+// } else {
+//     console.log('Valor menor ou igual a 20');
+// }
+
+// PAREI NA BOLINHA 5.
