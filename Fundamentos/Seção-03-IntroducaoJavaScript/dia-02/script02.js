4f54d97ac744ec0f9d601443bc8d224672712e93
Lianny Coringa <lianny_costa@hotmail.com>
2023-03-29 15:14:19 -0300

    Exercícios de loop for finalizados

--- Fundamentos/Seção-03-IntroducaoJavaScript/dia-02/script02.js
+++ Fundamentos/Seção-03-IntroducaoJavaScript/dia-02/script02.js
@@ -1,5 +1,8 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let sum = 0;
+let maiorNumber = 0;
+let menorNumber = numbers[0];
+let numberImpar = 0;
 
 
 // for (let number of numbers){
@@ -16,4 +19,39 @@ let sum = 0;
 //     console.log('Valor menor ou igual a 20');
 // }
 
-// PAREI NA BOLINHA 5.
+// for (let index = 0; index < numbers.length; index += 1) {
+//     if (numbers[index] > maiorNumber){
+//         maiorNumber = numbers[index];
+//     };
+// };
+// console.log(maiorNumber)
+
+
+// for (let index = 0; index < numbers.length; index += 1) {
+//     if(numbers[index] % 2 !== 0){
+//         numberImpar += 1;
+//     };
+// };
+
+// if (numberImpar <= 0){
+//     console.log('Nenhum valor ímpar encontrado');
+// } else if (numberImpar > 0){
+//     console.log(numberImpar);
+// }
+
+// for (let index = 1; index < numbers.length; index += 1){
+//     if (numbers[index] < menorNumber){
+//         menorNumber = numbers[index];
+//     };
+// }; 
+// console.log(menorNumber)
+
+let meuArray = [];
+for (let index = 1; index <= 25; index += 1) {
+    meuArray.push(index);
+};
+
+for (let index2 = 0; index2 < meuArray.length; index2 += 1){
+    console.log(meuArray[index2] / 2)
+}
+// console.log(meuArray)
