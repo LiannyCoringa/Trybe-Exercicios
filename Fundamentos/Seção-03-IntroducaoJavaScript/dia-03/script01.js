808280cd826c67ae5c30b4e51d00efa32066c1c2
Lianny Coringa <lianny_costa@hotmail.com>
2023-03-30 11:18:29 -0300

    Exercício dois sobre parâmetros finalizado

--- Fundamentos/Seção-03-IntroducaoJavaScript/dia-03/script01.js
+++ Fundamentos/Seção-03-IntroducaoJavaScript/dia-03/script01.js
@@ -7,5 +7,17 @@ function clientes(name) {
     } else {
         return `O nome precisa ser uma String.`;
 }
-} console.log(clientes(5))
+}
+// console.log(clientes(5))
+// console.log(clientesTrybeBank)
+
+function removeClientes(name) {
+    if (typeof name === 'string' && clientesTrybeBank.includes(name)){
+        clientesTrybeBank.splice(clientesTrybeBank.indexOf(name), 1);
+        return `Cliente removido com sucesso!`
+    } else {
+        return `O nome precisa ser uma String e/ou nome não existente.`;
+}
+} 
+console.log(removeClientes('Juliana'))
 console.log(clientesTrybeBank)
