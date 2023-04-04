5668c647b59360f93c61a99266f06dd99cb308d0
Lianny Coringa <lianny_costa@hotmail.com>
2023-04-04 12:25:23 -0300

    Primeira parte dos Exercícios feitos

---
+++ Fundamentos/Seção-03-IntroducaoJavaScript/dia-05/README.md
@@ -0,0 +1,13 @@
+Descrição dos exercícios:
+
+1. script.js:
+    Modifique a variável para que respeite o escopo em que está sendo declarada. Lembre-se: para que a variável respeite o escopo, ela não deve ser acessível fora do escopo em que esteja sendo declarada.
+    Altere o valor das propriedades do objeto para que respeite as características da variável do tipo const.
+    Modifique a variável para que não ocorra Erro.
+    Modifique as concatenações para template literals.
+
+    Modifique a estrutura das funções a seguir para que elas sejam arrow functions.
+    Transforme a função numeroAleatorio em uma arrow function.
+    Transforme a função hello em uma arrow function.
+    Transforme a função nomeCompleto em uma arrow function.
+    Altere a expressão if/else utilizando ternary operator.
---
+++ Fundamentos/Seção-03-IntroducaoJavaScript/dia-05/script.js
@@ -0,0 +1,33 @@
+// function imprimeIdade() {
+//     for (let idade = 30; idade <= 40; idade += 1) {
+//       console.log('Idade dentro do for:', idade)
+//     }
+//   }
+//   imprimeIdade()
+
+
+// Executando esse código, recebe-se um erro `TypeError: Assignment to constant variable.`
+// const pessoa = {
+//     nome: 'Henri',
+//     idade: 20
+//   }
+//   pessoa.nome = 'Luna';
+//   pessoa.idade = 19;
+//   console.log('Nome:', pessoa.nome);
+//   console.log('Idade:', pessoa.idade);
+
+
+// let favoriteFood = 'Lasanha';
+// favoriteFood = 'Hambúrguer';
+// console.log(favoriteFood);
+
+const name = 'Adriana';
+const lastName = 'Soares';
+console.log(`Olá, ${name} ${lastName}!`);
+function soma(a,b) {
+  let resultado = a + b;
+  return resultado;
+}
+let a = 3;
+let b = 5;
+console.log(`O resultado da soma de ${a} + ${b} é ${soma(a,b)}`);
