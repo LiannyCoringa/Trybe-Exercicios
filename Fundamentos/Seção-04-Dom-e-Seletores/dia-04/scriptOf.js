73722f6a1490a8e0a7b0846dc3627ddd4cfef42c
Lianny Coringa <lianny_costa@hotmail.com>
2023-04-13 16:31:31 -0300

    ajeitando o backgroundColor e o Color

--- Fundamentos/Seção-04-Dom-e-Seletores/dia-04/scriptOf.js
+++ Fundamentos/Seção-04-Dom-e-Seletores/dia-04/scriptOf.js
@@ -1,22 +1,34 @@
-window.onload = () => {
   const backgroundColorButton = document.querySelectorAll('#background-color button');
   const backgroundColor = document.getElementById('background-color');
   const colors = document.querySelectorAll('#font-color button');
   const main = document.getElementsByTagName('main');
 
+  if (localStorage.getItem('backgroundColor') !== null) {
+    main[0].style.backgroundColor = JSON.parse(localStorage.getItem('backgroundColor'));
+  }
+  if (localStorage.getItem('color') !== null) {
+    main[0].style.color = JSON.parse(localStorage.getItem('color'));
+  }
+
   const backgroundColors = (event) => {
-    main[0].style.backgroundColor = event.target.innerText;
     localStorage.setItem('backgroundColor', JSON.stringify(event.target.innerText));
+    if (localStorage.getItem('backgroundColor') !== null) {
+        main[0].style.backgroundColor = JSON.parse(localStorage.getItem('backgroundColor'));
+      }
   }
   for (let index = 0; index < backgroundColorButton.length; index += 1) {
   backgroundColorButton[index].addEventListener('click', backgroundColors);
   }
 
+
+
   const fontColors = (event) => {
-    main[0].style.color = event.target.innerText;
     localStorage.setItem('color', JSON.stringify(event.target.innerText));
+    if (localStorage.getItem('color') !== null) {
+        main[0].style.color = JSON.parse(localStorage.getItem('color'));
+      }
   }
   for (let index = 0; index < colors.length; index += 1) {
   colors[index].addEventListener('click', fontColors);
   }
-}
+
