aae8162c79a3eef47c1ef9ad3a45d9cb7db102c3
Lianny Coringa <lianny_costa@hotmail.com>
2023-04-26 20:18:39 -0300

    Requisito 21 começando

--- Fundamentos/Seção-05-FormsFlexboxResponsivo/dia-06/script.js
+++ Fundamentos/Seção-05-FormsFlexboxResponsivo/dia-06/script.js
@@ -25,3 +25,31 @@ textarea[1].addEventListener('keydown', () => {
     let resta = max - caracteres;
     contador.innerHTML = resta;
 })
+
+const formInfos = document.querySelector('#form-data');
+const nome = document.querySelector('#input-name');
+const emails = document.querySelector('#input-email');
+const house = document.querySelector('#house');
+const familia = document.querySelector('input[name="family"]');
+const materias = document.querySelectorAll('.subject');
+const avaliação = document.querySelector('input[name="rate"]');
+const formulario = document.querySelector('#evaluation-form');
+const texto = document.querySelector('textarea');
+formInfos.style.display = 'none';
+
+buttonSubmit.addEventListener('click', (event) => {
+    event.preventDefault();
+    for (let index = 0; index < materias.length; index += 1) {
+        let materiasSelecionadas = '';
+        materiasSelecionadas += `${materias[index]}, `;
+        }
+
+        let infos = [`Nome: ${nome.value}`, `Email: ${emails.value}`, `Casa: ${optionSelected}`, `Família: ${familia.value}`, `Matérias: ${materiasSelecionadas}`, `Avaliação: ${avaliação}`, `Observações: ${texto.value}`];
+        for (let index = 0; index < infos.length; index += 1) {
+            const parag = document.createElement('p');
+            parag.innerText = infos[index];
+            formInfos.appendChild(parag);
+        }
+})
+// FALTA PEGAR OS SELECIONADOS;
+// FALTA DESCOBRIR COMO APAGAR O FORM E ACRESCENTAR O INPUT;
