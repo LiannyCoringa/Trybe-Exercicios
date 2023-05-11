003fa348657ea3e02abf987305c1d7b1c89332c5
Lianny Coringa <lianny_costa@hotmail.com>
2023-05-11 17:54:30 -0300

    Ultimos exercicios

--- Fundamentos/Secao-07-HOFs/dia-5/Exercicio.js
+++ Fundamentos/Secao-07-HOFs/dia-5/Exercicio.js
@@ -26,3 +26,35 @@ const greet = (nome, saudacao = 'Hi') => `${saudacao} ${nome}`;
 console.log(greet('John')); // 'Hi John'
 console.log(greet('John', 'Good morning')); // 'Good morning John'
 console.log(greet('Isabela', 'Oi')); // 'Oi Isabela'
+
+// Exercico 4:
+const student1 = {
+    name: `Claudia`,
+    lastName: `Farias`,
+    age: 23,
+  }
+
+  const student2 = {
+    name: `Vitor`,
+    age: 20,
+  }
+
+const getLastName = ({lastName = 'lastName nao preenchido'}) => lastName;
+
+  console.log(getLastName(student1));
+  console.log(getLastName(student2));
+
+// Exercicio 5:
+const moreStudents = [
+    'Chris',
+    ['Ahmad', 'Antigoni'],
+    ['Toby', 'Sam']
+  ];
+
+  // Escreva seu código aqui
+  const [student01, student02, student03] = moreStudents;
+  const [student04, student05] = student02;
+  const [student06, student07] = student03;
+
+
+console.log(student01, student04, student05, student06, student07);
